9359f7c0129a0da48f828e5bff805994edb9ee20
greywolf001au <ecowley@epcit.biz>
2013-03-03 08:09:37 +1100

    fix writeln 002

--- termhelper.js
+++ termhelper.js
@@ -69,7 +69,6 @@
             thlib.input.cursor_pos = 0;
             thlib.input.string = "";
             this.CursorTo(0);
-            this.Prompt();
         },
         Run: function (command) {
 			child = exec(command, // command line argument directly in string
